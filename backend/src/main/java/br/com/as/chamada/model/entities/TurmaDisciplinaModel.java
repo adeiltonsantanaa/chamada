9709fc4b261a5b48bae992beed5586d4e229ceaf
Adeilton Santana <filhoaguiar20@gmail.com>
2022-09-21 10:19:30 -0300

    criando relacionamentos dessas entidades com a entidade chamadaModel

--- backend/src/main/java/br/com/as/chamada/model/entities/TurmaDisciplinaModel.java
+++ backend/src/main/java/br/com/as/chamada/model/entities/TurmaDisciplinaModel.java
@@ -11,6 +11,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -30,6 +31,9 @@ public class TurmaDisciplinaModel {
 	@ManyToOne
 	@JoinColumn(name = "professor")
 	private ProfessorModel professor;
+	
+	@OneToMany(mappedBy = "turmaDiscModel")
+	List<ChamadaModel> chamadaModel;
 
 	@ManyToMany
 	@JoinTable(name = "tb_disciplina_aluno", 
