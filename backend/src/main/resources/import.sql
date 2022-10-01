1d26bdf213bf3ae860b4f8198ab90bfe9d2d10ea
Adeilton Santana <filhoaguiar20@gmail.com>
2022-10-01 10:57:25 -0300

    adicionando a inserção de dados via script

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,2 +1,4 @@
 INSERT INTO tb_professor(nome) VALUES ('lucas');
+INSERT INTO tb_aluno(matricula, nome) values (2021, 'adeilton');
 INSERT INTO tb_disciplina(disciplina_nome, turma_nome, professor) VALUES ('POO java', 'ara1001', 1);
+INSERT INTO tb_disciplina_aluno (desc_id, aluno_id) values (1,1);
