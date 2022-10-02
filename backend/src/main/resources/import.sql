5980b53c6c5d63b0191d1ee786395945a0e9e158
Adeilton Santana <filhoaguiar20@gmail.com>
2022-10-01 21:36:49 -0300

    adicionando mais inserts

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,4 +1,6 @@
 INSERT INTO tb_professor(nome) VALUES ('lucas');
 INSERT INTO tb_aluno(matricula, nome) values (2021, 'adeilton');
 INSERT INTO tb_disciplina(disciplina_nome, turma_nome, professor) VALUES ('POO java', 'ara1001', 1);
+INSERT INTO tb_disciplina(disciplina_nome, turma_nome, professor) VALUES ('POO 2', 'ara1001', 1);
+INSERT INTO tb_disciplina(disciplina_nome, turma_nome, professor) VALUES ('POO 3', 'ara1001', 1);
 INSERT INTO tb_disciplina_aluno (desc_id, aluno_id) values (1,1);
