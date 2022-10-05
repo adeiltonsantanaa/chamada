26293985a616b20affda430d8d06883513df0d8b
Adeilton Santana <filhoaguiar20@gmail.com>
2022-10-05 14:14:05 -0300

    criação de novos inserts

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -4,3 +4,5 @@ INSERT INTO tb_disciplina(disciplina_nome, turma_nome, professor) VALUES ('POO j
 INSERT INTO tb_disciplina(disciplina_nome, turma_nome, professor) VALUES ('POO 2', 'ara1001', 1);
 INSERT INTO tb_disciplina(disciplina_nome, turma_nome, professor) VALUES ('POO 3', 'ara1001', 1);
 INSERT INTO tb_disciplina_aluno (desc_id, aluno_id) values (1,1);
+INSERT INTO tb_user(id, username, password) values (1,'adeilton', '$2a$10$4s.IyM8ItjnTfPZnPreigeQsy3vizCjtrQLRq.X4.8aTby2KJUDHK');
+INSERT INTO tb_user_role VALUES (1, 'ROLE_ADMIN')
