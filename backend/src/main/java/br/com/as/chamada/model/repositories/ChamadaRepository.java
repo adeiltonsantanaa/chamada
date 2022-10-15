ee329c0f9a05ecd610c842c5c7f10bb4396b4a16
Adeilton <filhoaguiar20@gmail.com>
2022-10-14 21:42:27 -0300

    criação de uma consulta para trazer todas as chamadas

--- backend/src/main/java/br/com/as/chamada/model/repositories/ChamadaRepository.java
+++ backend/src/main/java/br/com/as/chamada/model/repositories/ChamadaRepository.java
@@ -1,11 +1,17 @@
 package br.com.as.chamada.model.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.as.chamada.model.entities.ChamadaModel;
 
 @Repository
 public interface ChamadaRepository extends JpaRepository<ChamadaModel, Long> {
+
+	@Query("SELECT c FROM ChamadaModel c")
+	List<ChamadaModel> buscarTodas();
 
 }
