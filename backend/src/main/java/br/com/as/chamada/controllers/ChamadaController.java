70282ebf318af26614ac66cccf8e46b440ef6efb
Adeilton <filhoaguiar20@gmail.com>
2022-10-17 22:03:09 -0300

    liberando cors no controler

--- backend/src/main/java/br/com/as/chamada/controllers/ChamadaController.java
+++ backend/src/main/java/br/com/as/chamada/controllers/ChamadaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import br.com.as.chamada.model.entities.DisciplinaModel;
 import br.com.as.chamada.model.services.ChamadaService;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(value = "api/v1/chamada")
 public class ChamadaController {
 
