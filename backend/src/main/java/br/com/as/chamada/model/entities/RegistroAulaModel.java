c948bb296756deb196e54df29af1648f8163c338
Adeilton <filhoaguiar20@gmail.com>
2022-10-19 18:31:49 -0300

    criação da entidade de registro de aulas

---
+++ backend/src/main/java/br/com/as/chamada/model/entities/RegistroAulaModel.java
@@ -0,0 +1,86 @@
+package br.com.as.chamada.model.entities;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_registro_aula")
+public class RegistroAulaModel implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String descricao;
+
+	private LocalDate dataRegistro;
+
+	@ManyToOne
+	@JoinColumn(name = "professor")
+	private ProfessorModel professor;
+
+	@ManyToOne
+	@JoinColumn(name = "fk_turmaDisc")
+	private DisciplinaModel discModel;
+
+	public RegistroAulaModel() {
+	}
+
+	public RegistroAulaModel(String descricao, LocalDate dataRegistro, ProfessorModel professor,
+			DisciplinaModel discModel) {
+		this.descricao = descricao;
+		this.dataRegistro = dataRegistro;
+		this.professor = professor;
+		this.discModel = discModel;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public LocalDate getDataRegistro() {
+		return dataRegistro;
+	}
+
+	public void setDataRegistro(LocalDate dataRegistro) {
+		this.dataRegistro = dataRegistro;
+	}
+
+	public ProfessorModel getProfessor() {
+		return professor;
+	}
+
+	public void setProfessor(ProfessorModel professor) {
+		this.professor = professor;
+	}
+
+	public DisciplinaModel getDiscModel() {
+		return discModel;
+	}
+
+	public void setDiscModel(DisciplinaModel discModel) {
+		this.discModel = discModel;
+	}
+
+}
