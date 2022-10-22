7294c0ce35bb53cada7e55aad79f4b42992f09ff
Adeilton <filhoaguiar20@gmail.com>
2022-10-21 21:41:41 -0300

    tirando o console.log

--- frontend/src/uteis/getSaudacao.js
+++ frontend/src/uteis/getSaudacao.js
@@ -1,6 +1,5 @@
 export function saudacao(){
     let hora = new Date().getHours(); 
-    console.log(hora)
         if(hora >= 6 && hora <= 12) {return "Boa Dia! "}
         else if (hora >= 13 && hora <= 18) {return "Boa Tarde!"}
         else if (hora > 18 && hora <= 23) {return "Boa Noite! "}
