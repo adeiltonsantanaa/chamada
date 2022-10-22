be8f5c7f1779b7774710fd555158cd3bc1194c5d
Adeilton <filhoaguiar20@gmail.com>
2022-10-21 21:37:32 -0300

    criação das classes de request e response para registro de aula

---
+++ backend/src/main/java/br/com/as/chamada/model/vo/RegistroAulaResponseVO.java
@@ -0,0 +1,69 @@
+package br.com.as.chamada.model.vo;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class RegistroAulaResponseVO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String descricao;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+	private LocalDate dataRegistro;
+	private String professor;
+	private String discModel;
+
+	public RegistroAulaResponseVO() {
+	}
+
+	public RegistroAulaResponseVO(Long id, String descricao, LocalDate dataRegistro, String professor, String discModel) {
+		this.id = id;
+		this.descricao = descricao;
+		this.dataRegistro = dataRegistro;
+		this.professor = professor;
+		this.discModel = discModel;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public LocalDate getDataRegistro() {
+		return dataRegistro;
+	}
+
+	public void setDataRegistro(LocalDate dataRegistro) {
+		this.dataRegistro = dataRegistro;
+	}
+
+	public String getProfessor() {
+		return professor;
+	}
+
+	public void setProfessor(String professor) {
+		this.professor = professor;
+	}
+
+	public String getDiscModel() {
+		return discModel;
+	}
+
+	public void setDiscModel(String discModel) {
+		this.discModel = discModel;
+	}
+
+}
