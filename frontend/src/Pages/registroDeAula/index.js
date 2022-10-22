29e6a63ce2381bd84b298511d35044b0f1c33988
Adeilton <filhoaguiar20@gmail.com>
2022-10-21 21:41:27 -0300

    fazendo alterações nos nomes dos estados

--- frontend/src/Pages/registroDeAula/index.js
+++ frontend/src/Pages/registroDeAula/index.js
@@ -8,10 +8,11 @@ import './styles.css';
 export default function RegistroAula() {
 
     const [turma, setTurma] = useState([]);
-    const [disciplina, setDisciplina] = useState(1);
-    const [matricula, setMatricula] = useState();
-    const [data, setData] = useState(Date.now());
-    const [professor, setProfessor] = useState('');
+
+    const [discModel, setDiscModel] = useState(1);
+    const [dataRegistro, setDataRegistro] = useState(new Date());
+    const [professor, setProfessor] = useState(1);
+    const [descricao, setDescricao] = useState('');
 
     useState(() => {
         api.get('/api/v1/chamada/buscar/turmas')
@@ -19,17 +20,16 @@ export default function RegistroAula() {
             .catch(err => console.log(err));
     }, [])
 
+    console.log(turma)
     function salvaChamada() {
-            api.post('/api/v1/chamada/salvar', {
-                disciplina: disciplina,
-                matricula: matricula,
-            }).then(res => alert("Presença Registrada!"))
-            .catch(err => alert(err.response.data.message))
-    }
-
-    function teste(e) {
-        e.preventDefault()
-        console.log(data)
+        const token = localStorage.getItem('accessToken');
+        api.post('/api/v1/registro', {
+            descricao: descricao,
+            professor: professor,
+            discModel: discModel,
+            dataRegistro: dataRegistro
+        }, { headers: { Authorization: 'Bearer ' + token } }).then(res => alert("Presença Registrada!"))
+            .catch(err => console.log(err))
     }
 
     return (
@@ -47,26 +47,26 @@ export default function RegistroAula() {
                     <form>
                         <input
                             placeholder="Descrição da Aula"
-                            value={matricula}
-                            onChange={e => setMatricula(e.target.value)}
+                            value={descricao}
+                            onChange={e => setDescricao(e.target.value)}
                         />
                         <input
                             placeholder="Professor"
                             value={professor}
-                            onChange={e => setMatricula(e.target.value)}
+                            onChange={e => setProfessor(e.target.value)}
                         />
                         <input
                             placeholder="Data"
                             type={"date"}
-                            value={data}
-                            onChange={e => setData(e.target.value)}
+                            value={dataRegistro}
+                            onChange={e => setDataRegistro(e.target.value)}
                         />
-                        <select onChange={e => setDisciplina(e.target.value)}>
+                        <select onChange={e => setDiscModel(e.target.value)}>
                             {turma.map(turma => {
                                 return <option key={turma.id} value={turma.id}>{turma.disciplinaNome}</option>
                             })}
                         </select>
-                        <button onClick={teste} className="button" type="button">Registrar</button>
+                        <button onClick={salvaChamada} className="button" type="button">Registrar</button>
                     </form>
                 </div>
             </div>
