e4eeb7b1f778e732bf2ab52ba89809673ee31111
Adeilton <filhoaguiar20@gmail.com>
2022-10-24 16:52:05 -0300

    refatoração da classe vo response de chamada

--- backend/src/main/java/br/com/as/chamada/model/vo/ChamadaRequestVO.java
+++ backend/src/main/java/br/com/as/chamada/model/vo/ChamadaRequestVO.java
@@ -3,24 +3,21 @@ package br.com.as.chamada.model.vo;
 import java.io.Serializable;
 import java.util.Date;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-public class ChamadaVO implements Serializable {
+public class ChamadaRequestVO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
 	private Long matricula;
-	@JsonBackReference
 	private Long disciplina;
-	private String disciplinaNome;
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
 	private Date datetime;
 
-	public ChamadaVO() {
+	public ChamadaRequestVO() {
 	}
 
-	public ChamadaVO(Long id, Long matricula, Long disciplina, Date datetime) {
+	public ChamadaRequestVO(Long id, Long matricula, Long disciplina, Date datetime) {
 		this.id = id;
 		this.matricula = matricula;
 		this.disciplina = disciplina;
@@ -33,14 +30,6 @@ public class ChamadaVO implements Serializable {
 
 	public void setId(Long id) {
 		this.id = id;
-	}
-
-	public String getDisciplinaNome() {
-		return disciplinaNome;
-	}
-
-	public void setDisciplinaNome(String disciplinaNome) {
-		this.disciplinaNome = disciplinaNome;
 	}
 
 	public Long getMatricula() {
