3fdfd732bdf5ec127733bc2a86972ef63bb69896
Adeilton <filhoaguiar20@gmail.com>
2022-10-24 19:38:59 -0300

    criação de uma função de validação de inputs

--- backend/src/main/java/br/com/as/chamada/model/services/AlunoService.java
+++ backend/src/main/java/br/com/as/chamada/model/services/AlunoService.java
@@ -1,8 +1,11 @@
 package br.com.as.chamada.model.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+import br.com.as.chamada.exceptions.NullInputException;
 import br.com.as.chamada.model.entities.AlunoModel;
 import br.com.as.chamada.model.repositories.AlunoRepository;
 import br.com.as.chamada.model.vo.AlunoVO;
@@ -13,9 +16,20 @@ public class AlunoService {
 	@Autowired
 	private AlunoRepository alunoRepository;
 
+	@ResponseStatus(code = HttpStatus.OK)
 	public AlunoVO salvaAluno(AlunoModel aluno) {
+		validaEntradas(aluno);
 		alunoRepository.save(aluno);
 		return AlunoVO.parseToVO(aluno);
+	}
+
+	private boolean validaEntradas(AlunoModel aluno) throws NullInputException {
+		if (aluno.getMatricula() == null) {
+			throw new NullInputException("A matrícula do aluno não pode ser nula!");
+		} else if (aluno.getNome() == null) {
+			throw new NullInputException("O nome do aluno não pode ser nulo!");
+		}
+		return true;
 	}
 
 }
